b13c2b2087aee3719d15a899d2cb8f43c5d1745f
Gurmessa <gurmessa@prompt.com>
2025-06-07 12:31:28 +0300

    create view for home page and testing

--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import PortfolioProfile, TechStackCategory
 
-# Create your views here.
+def home(request):
+    portfolio_profile = PortfolioProfile.get_solo()
+    tech_stack_categories = TechStackCategory.objects.all().prefetch_related('tech_stacks')
+    return render(request, 'core/home.html', {
+        'portfolio_profile': portfolio_profile,
+        'tech_stack_categories': tech_stack_categories
+    })
