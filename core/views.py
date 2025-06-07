4a4deb724c752de02acb883b92dab760a8ceebf4
Gurmessa <gurmessa@prompt.com>
2025-06-07 14:48:52 +0300

    add experiences_data_json home view context

--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
+import json
 from django.shortcuts import render
-from .models import PortfolioProfile, TechStackCategory
+from .models import PortfolioProfile, TechStackCategory, WorkExperience
+from core.utils import get_experiences_data_json
 
 
 def home(request):
@@ -7,11 +9,18 @@ def home(request):
     tech_stack_categories = TechStackCategory.objects.all().prefetch_related(
         "tech_stacks"
     )
+
+    work_experiences = WorkExperience.objects.prefetch_related(
+        "items", "tech_stacks"
+    ).all()
+    experiences_data_json = get_experiences_data_json(work_experiences)
+
     return render(
         request,
         "core/home.html",
         {
             "portfolio_profile": portfolio_profile,
             "tech_stack_categories": tech_stack_categories,
+            "experiences_data_json": experiences_data_json,
         },
     )
