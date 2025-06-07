005881f51c433209154bd7df5d4d3724ffe32a39
Gurmessa <gurmessa@prompt.com>
2025-06-07 13:06:49 +0300

    formatting on core app

--- core/views.py
+++ core/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from .models import PortfolioProfile, TechStackCategory
 
+
 def home(request):
     portfolio_profile = PortfolioProfile.get_solo()
-    tech_stack_categories = TechStackCategory.objects.all().prefetch_related('tech_stacks')
-    return render(request, 'core/home.html', {
-        'portfolio_profile': portfolio_profile,
-        'tech_stack_categories': tech_stack_categories
-    })
+    tech_stack_categories = TechStackCategory.objects.all().prefetch_related(
+        "tech_stacks"
+    )
+    return render(
+        request,
+        "core/home.html",
+        {
+            "portfolio_profile": portfolio_profile,
+            "tech_stack_categories": tech_stack_categories,
+        },
+    )
