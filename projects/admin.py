1f36d960b54c40684864c97963f5a0d4e1716af3
Gurmessa <gurmessa@prompt.com>
2025-06-08 08:48:04 +0300

    create admin for Project

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Project, ProjectKeyFeature
 
-# Register your models here.
+class ProjectKeyFeatureInline(admin.TabularInline):
+    model = ProjectKeyFeature
+    extra = 1
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    inlines = [ProjectKeyFeatureInline]
+    list_display = ('name', 'start_date', 'end_date', 'is_open_source')
+    search_fields = ('name', 'description')
+    list_filter = ('tech_stacks', 'start_date', 'end_date')
