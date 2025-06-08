a6876435ea59074c723dda124489aeded3fd34c7
Gurmessa <gurmessa@prompt.com>
2025-06-08 12:24:20 +0300

    add thumbail for project image

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third-party apps
     'ckeditor',
     'solo',
+    'sorl.thumbnail',
+    # Project-specific apps
     'core',
     'blogs',
     'projects',
