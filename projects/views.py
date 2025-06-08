e09997e06cd969bd68013f79c7079db32a70a788
Gurmessa <gurmessa@prompt.com>
2025-06-08 12:49:54 +0300

    filter project by tech stack

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView, DetailView
 from .models import Project
+from projects.filters import ProjectFilter
 
 
 class ProjectListView(ListView):
@@ -8,7 +9,18 @@ class ProjectListView(ListView):
     context_object_name = "projects"
 
     def get_queryset(self):
-        return Project.objects.all().prefetch_related("tech_stacks")
+        queryset = super().get_queryset().prefetch_related("tech_stacks")
+        self.filterset = ProjectFilter(self.request.GET, queryset=queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # Pass width to template (with default value 0)
+        tech_stack = self.request.GET.get("tech_stack", "All")
+
+        context["tech_stack"] = tech_stack
+        return context
 
 
 class ProjectDetailView(DetailView):
