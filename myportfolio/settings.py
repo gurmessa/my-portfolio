502a9efdee52864283effcafd96eb243e506d4d1
Gurmessa <gurmessa@prompt.com>
2025-06-10 08:12:20 +0300

    --amend-no-edit

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -72,7 +72,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                'core.context_processors.global_object',
+                "core.context_processors.global_object",
             ],
         },
     },
