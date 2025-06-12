546328ed5655cac16320c9d25cf87d7ca1feddb0
Gurmessa <gurmessa@prompt.com>
2025-06-12 12:14:49 +0300

    small fix

--- blogs/models.py
+++ blogs/models.py
@@ -16,7 +16,9 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
-
+    
+    class Meta:
+        verbose_name_plural = "Blog Categories"
 
 class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True)
